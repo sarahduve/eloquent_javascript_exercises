7fa239498ba636f4322b06e7117c579930d68fe7
Sarah Duve <saduve@gmail.com>
2019-08-29 12:29:35 -0400

    Prepend and nth

--- 4_data_structures/list.js
+++ 4_data_structures/list.js
@@ -15,20 +15,20 @@ console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
 
 // loop version
-// function arrayToList(array) {
-  // let obj = {"value": array.pop(), "rest": null};
-  // while (array.length > 0) {
-    // value = array.pop();
-    // obj = {"value": value, "rest": obj};
-  // }
-  // return obj;
-// }
+function loopArrayToList(array) {
+  let obj = {"value": array.pop(), "rest": null};
+  while (array.length > 0) {
+    value = array.pop();
+    obj = {"value": value, "rest": obj};
+  }
+  return obj;
+}
 
 // recursive version
 function arrayToList(array, rest=null) {
@@ -46,5 +46,17 @@ function listToArray(list, values=[]) {
   } else {
     values.push(list.value);
     return listToArray(list.rest, values);
+  }
+}
+
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, index, currentIndex=0) {
+  if (currentIndex === index) {
+    return list.value;
+  } else {
+    return nth(list.rest, index, currentIndex + 1);
   }
 }
